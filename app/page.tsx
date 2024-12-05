ba465d65ef4683f07d38c0a290a10b7b5d0cf2ab
dykenson <jdlaurent@haitipay.ht>
2024-12-05 14:37:37 -0500

    main

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
-import Hero from "@/components/Hero";
+import Banner from './components/Banner/Banner';
+import Companies from './components/Companies/Companies';
+import Buyers from './components/Buyers/index';
+import Provide from './components/Provide/index';
+import Why from './components/Why/index';
+import Network from './components/Network/index';
+import Clientsay from './components/Clientsay/index';
+import Newsletter from './components/Newsletter/Newsletter';
+
 
 export default function Home() {
   return (
-    <Hero/>
-  );
+    <main>
+      <Banner />
+      <Companies />
+      <Buyers />
+      <Provide />
+      <Why />
+      <Network />
+      <Clientsay />
+      <Newsletter />
+    </main>
+  )
 }
